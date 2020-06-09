6ac1c94d5a616d721728eeae760e0a2ccc8b0349
sfertman <sfertman@zoho.com>
2020-06-09 14:41:49 -0400

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ certbot "${ARGS[*]:0:1}" \
   --config-dir  /usr/local/letsencrypt/config/ \
   --work-dir    /usr/local/letsencrypt/work/ \
   --server https://acme-v02.api.letsencrypt.org/directory \
-  "${ARGS[*]:1}"
+  ${ARGS[*]:1}
